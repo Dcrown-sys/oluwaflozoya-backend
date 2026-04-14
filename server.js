044acf5475206e48edae7bc818ed60d5e77e34b2
crown <blakekolberg41@gmail.com>
2026-04-14 05:22:49 -0700

    🔥 Fix req.body undefined - add express.json middleware

--- server.js
+++ server.js
@@ -48,6 +48,11 @@ app.use(cors({
 }));
 
 
+app.use(express.json({ limit: '10mb' }));
+app.use(express.urlencoded({ extended: true, limit: '10mb' }));
+app.use(express.raw({ type: 'application/json', limit: '10mb' }));
+
+
 
 
 // Request logger
