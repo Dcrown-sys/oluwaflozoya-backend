5a44412df92e80d98f509a9fb04814d3103186df
crown <blakekolberg41@gmail.com>
2026-05-13 07:58:18 -0700

    i made new changes

--- middleware/errorHandler.js
+++ middleware/errorHandler.js
@@ -1,12 +1,16 @@
-// middleware/errorHandler.js
 exports.errorHandler = (err, req, res, next) => {
-    if (err && err.status) {
-      return res.status(err.status).json({
-        success: false,
-        message: err.message,
-        details: err.details ?? undefined,
-      });
-    }
-    console.error('[unhandled]', err);
-    res.status(500).json({ success: false, message: 'Internal server error' });
-  };
+  if (err && err.status) {
+    return res.status(err.status).json({
+      success: false,
+      message: err.message,
+      details: err.details ?? undefined,
+    });
+  }
+  console.error('[unhandled]', err);
+  res.status(500).json({ 
+    success: false, 
+    message: 'Internal server error',
+    error: err.message,        // ADD
+    stack: err.stack           // ADD
+  });
+};
